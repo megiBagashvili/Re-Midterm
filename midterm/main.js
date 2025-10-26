a93c93feffc89c8d91ba4764fbae206654a25416
megiBagashvili <megibagashvili2004@gmail.com>
2025-10-26 19:18:28 +0400

    added update functionality

--- midterm/main.js
+++ midterm/main.js
@@ -16,7 +16,7 @@ program
     .option('-c, --category <category>', 'shows expenses that match given category', '')
     .description('Show all expenses from expenses.json')
     .action(async (opts) => {
-        const expenseData = await readFile('./expenses.json', true);
+        const expenseData = await readFile('expenses.json', true);
 
         if (opts.category) {
             const filteredData = expenseData.filter(expenseCategory => expenseCategory.category === opts.category);
@@ -46,7 +46,7 @@ program
     .argument('<category>', 'expense type')
     .argument('<price>', 'expense total cost')
     .action(async (category, price) => {
-        const currentExpenses = await readFile('./expenses.json', true);
+        const currentExpenses = await readFile('expenses.json', true);
         const lastId = currentExpenses[currentExpenses.length - 1]?.id || 0;
         price = Number(price);
 
@@ -63,7 +63,7 @@ program
 
         }
         currentExpenses.push(newExpense);
-        await writeFile('./expenses.json', currentExpenses);
+        await writeFile('expenses.json', currentExpenses);
 
     })
 
@@ -72,14 +72,14 @@ program
     .description('deletes an expense by id from the expenses file')
     .argument('<id>', 'id of expense to be deleted')
     .action(async (id) => {
-        const currentExpenses = await readFile('./expenses.json', true);
+        const currentExpenses = await readFile('expenses.json', true);
         const expenseToBeDeleted = currentExpenses.find(exp => exp.id === Number(id));
         if (!expenseToBeDeleted) {
             console.log(`Eexpense with id ${id} not found.`);
             return;
         }
         const updatedExpenses = currentExpenses.filter(el => el !== expenseToBeDeleted);
-        await writeFile('./expenses.json', updatedExpenses);
+        await writeFile('expenses.json', updatedExpenses);
     })
 
 program
@@ -87,7 +87,7 @@ program
     .description('retrieves element matching the given id')
     .argument('<id>', 'id of expense')
     .action(async (id) => {
-        const currentExpenses = await readFile('./expenses.json', true);
+        const currentExpenses = await readFile('expenses.json', true);
         const targetExpense = currentExpenses.find(exp => exp.id === Number(id));
         if (!targetExpense) {
             console.log(`Expense with id ${id} not found.`);
@@ -102,13 +102,43 @@ program
     .description('searches expenses by date created')
     .argument('<date>', 'date to search expenses by (YYYY-MM-DD)')
     .action(async (date) => {
-        const currentExpenses = await readFile('./expenses.json', true);
+        const currentExpenses = await readFile('expenses.json', true);
         const targetExpenses = currentExpenses.filter(exp => exp.createdAt.startsWith(date));
         if (targetExpenses.length === 0) {
             console.log(`No expenses found for date ${date}.`);
             return;
         }
         console.log(targetExpenses);
+    })
+
+
+program
+    .command('update')
+    .description('updates expense specified by id')
+    .argument('<id>', 'id of expense to be updated')
+    .option('-c, --category <category>', 'new category')
+    .option('-p, --price <price>', 'new price')
+    .action(async (id, opts) => {
+        const currentExpenses = await readFile('expenses.json', true);
+        const indexToUpdate = currentExpenses.findIndex(exp => exp.id === Number(id));
+        if (indexToUpdate === -1) {
+            console.log(`Expense with id ${id} not found.`);
+            return;
+        }
+        const updateReq = {}
+        if(opts.category){
+            updateReq['category'] = opts.category
+        }
+        if(opts.price){
+            updateReq['price'] = Number(opts.price)
+        }
+        currentExpenses[indexToUpdate] = {
+            ...currentExpenses[indexToUpdate],
+            ...updateReq
+        }
+        await writeFile('expenses.json', currentExpenses);
+        console.log(`Expense with id ${id} has been updated.`);
+
     })
 
 program.parse();
